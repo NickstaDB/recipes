dae114fcae8723c362c0ad4210e31aea41ecc6d8
nickyb <nick@cognitous.co.uk>
2025-01-09 22:56:01 +0000

    Improved ingredient component by rendering ingredient notes.

--- vue/src/components/IngredientComponent.vue
+++ vue/src/components/IngredientComponent.vue
@@ -25,17 +25,9 @@
                     <a :href="ingredient.food.url" v-else-if="ingredient.food.url !== ''" target="_blank" rel="noopener noreferrer">
                         {{ foodName }}</a>
                     <template v-else>
-                        <span :class="foodClass">{{ foodName }}</span>
+                        <span class="d-block" :class="foodClass">{{ foodName }}</span>
+                        <span class="text-muted" v-if="ingredient.note">{{ ingredient.note }}</span>
                     </template>
-                </template>
-            </td>
-            <td v-if="detailed" class="ingredients__note align-baseline">
-                <template v-if="ingredient.note">
-                    <span class="ingredients__note ingredients__note_hover d-print-none touchable py-0 px-2" v-b-popover.hover="ingredient.note">
-                        <i class="far fa-comment"></i>
-                    </span>
-
-                    <div class="ingredients__note ingredients__note_print d-none d-print-block"><i class="far fa-comment-alt d-print-none"></i> {{ ingredient.note }}</div>
                 </template>
             </td>
         </template>
